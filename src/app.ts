ae5e24ec412b3e569029e254e381c03d8cd74364
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-10-20 14:04:03 -0300

    feat: create app

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,36 @@
-import express from 'express';
+import 'dotenv/config';
 
-export const app = express();
+import http from 'http'
+import express, { json } from 'express';
+import cors from 'cors'
+import { Server } from 'socket.io'
+
+import { routes } from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+app.use(routes);
+
+app.get('/auth/github', (_, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_OAUTH_CLIENT_ID}`,
+  );
+});
+
+app.get('/auth/github/callback', (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  }
+});
+
+export { httpServer, io };

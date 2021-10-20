fee07bb3933d2c8002cc86c4137668e3dc23d6ac
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-10-20 13:54:42 -0300

    feat: add get user profile service

---
+++ src/services/GetUserProfileService.ts
@@ -0,0 +1,19 @@
+import prismaClient from "../prisma"
+
+type Request = {
+  user_id: string;
+}
+
+export class GetUserProfileService {
+  async execute(data: Request) {
+    const { user_id } = data;
+
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: user_id,
+      }
+    });
+
+    return user;
+  }
+}

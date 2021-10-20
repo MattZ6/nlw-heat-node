c416bdd52dab1ac4bc6861f8ca2a4eaa8b40d29f
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-10-20 15:15:15 -0300

    chore: add index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { app } from './app';
+import { httpServer, io } from './app';
 
-app.listen(
+httpServer.listen(
   3333,
   () => console.log('🚀 Server is running on http://localhost:3333'),
 );
+
+io.on('connection', socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
